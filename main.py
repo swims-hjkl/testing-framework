8f368bb3a5fce701233bb8404cd9cc545cdc6aa6
swims_hjkl <sumedhrao7@gmail.com>
2025-05-26 22:15:34 +0530

    test file artifact

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ backend_url = "http://localhost:5002"
 def main():
     response = requests.get(backend_url)
     if response.text != "hello":
-        print("error: not passing")
+        with open("results.txt", "w"):
+            print("test case 1: failed")
     else:
-        print("pass")
+        with open("results.txt", "w"):
+            print("test case 1: passed")
 
 
 if __name__ == "__main__":
